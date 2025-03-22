39f4f50178eb38c71954b85d5696aa0b7e75e1f6
Sunny Au <sunny.au@outlook.com>
2025-03-22 19:46:16 +0000

    run a web browser and connect to mongodb database on ubuntu server

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # mongodb-js
 
-Following this youtube https://www.youtube.com/watch?v=a3H9rIj07sk&list=PL4cUxeGkcC9h77dJ-QJlwGlZlTd4ecZOA&index=15
+
+
+
+This tutorial follows these youtube
+
+- https://www.youtube.com/watch?v=a3H9rIj07sk&list=PL4cUxeGkcC9h77dJ-QJlwGlZlTd4ecZOA&index=15
+- https://www.youtube.com/watch?v=gGNquGHqpNI&list=PL4cUxeGkcC9h77dJ-QJlwGlZlTd4ecZOA&index=16
+- https://www.youtube.com/watch?v=5a6h41PVsD0&list=PL4cUxeGkcC9h77dJ-QJlwGlZlTd4ecZOA&index=17
 
 ## How to run
 
-npm run server
+`npm run server`
+
+```
+Node.js v20.12.2
+[nodemon] app crashed - waiting for file changes before starting...
+[nodemon] restarting due to changes...
+[nodemon] starting `node app.js`
+app listening on port 3000
+```
 
 ## How to view data on web browser
 
 http://localhost:3000/books
 
 
+
+## Where is my mongodb database ?
+
+It is running on ubuntu server with an ip address of 192.168.0.10
+
+Tutorial : https://www.youtube.com/watch?v=SNgaUYu5o1o 
--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
 
 // init app & middleware
 const app = express()
 
-app.listen(3000, () => {
-    console.log('app listening on port 3000')
+// db connection
+let db
+
+connectToDb( (err) => {
+    if(!err) {
+        app.listen(3000, () => {
+            console.log('app listening on port 3000')
+        })
+        db = getDb()
+    }
 })
 
 // routes
 app.get('/books', (req, res) => {
-    res.json({mssg: "welcome to the api"})
+    let books = []
+    // In shell, we use db.books
+
+    // find() returns a cursor toArray forEach
+    db.collection('books')
+        .find()
+        .sort({ author: 1})
+        .forEach( book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch( () => {
+            res.status(500).json({error: 'Could not fetch the documents'})
+        })
+    
+    // res.json({mssg: "welcome to the api"})
 })
